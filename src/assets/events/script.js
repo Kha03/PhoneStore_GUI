3a11b49b2047a73eca19a033c80a76b4bf9a407c
Kha <minhkha20031996@gmail.com>
2024-10-17 17:01:31 +0700

    filter product list

---
+++ src/assets/events/script.js
@@ -0,0 +1,54 @@
+const toggleButton = document.getElementById("toggleButton");
+const arrowIcon = document.getElementById("arrowIcon");
+
+toggleButton.addEventListener("click", () => {
+  arrowIcon.classList.toggle("arrow-up");
+});
+// Khởi tạo thanh trượt
+window.onload = function () {
+  var slider = document.getElementById("slider-range");
+
+  if (slider) {
+    // check slider
+    noUiSlider.create(slider, {
+      start: [7, 15], //intial value
+      connect: true,
+      range: {
+        min: 0,
+        max: 100,
+      },
+      step: 1,
+      tooltips: [false, false],
+    });
+
+    var minValue = document.getElementById("min-value");
+    var maxValue = document.getElementById("max-value");
+
+    slider.noUiSlider.on("update", function (values, handle) {
+      if (handle === 0) {
+        minValue.innerHTML = Math.round(values[0]);
+      } else {
+        maxValue.innerHTML = Math.round(values[1]);
+      }
+    });
+  }
+};
+
+const checkboxes = document.querySelectorAll('input[name="priceRange"]');
+const sliderPrice = document.getElementById("sliderPrice");
+const sliderLabel = document.querySelector('label[for="slider-range"]');
+
+// hide slider if any checkbox is selected
+checkboxes.forEach((checkbox) => {
+  checkbox.addEventListener("change", function () {
+    if (Array.from(checkboxes).some((checkbox) => checkbox.checked)) {
+      sliderPrice.classList.remove("show");
+    }
+  });
+});
+// uncheck all checkboxes when slider is shown
+sliderLabel.addEventListener("click", function () {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  });
+});
