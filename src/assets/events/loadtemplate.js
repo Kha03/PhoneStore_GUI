fa5a5552760fcfdd831cf84132b74d820f05dad0
Kha <minhkha20031996@gmail.com>
2024-11-14 13:37:47 +0700

    change src and update account sign in sign up

---
+++ src/assets/events/loadtemplate.js
@@ -0,0 +1,30 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+/**
+ * Hàm tải template
+ *
+ * Cách dùng:
+ * <div id="parent"></div>
+ * <script>
+ *  load("#parent", "./path-to-template.html");
+ * </script>
+ */
+function load(selector, path) {
+  const cached = localStorage.getItem(path);
+  if (cached) {
+    $(selector).innerHTML = cached;
+  }
+
+  fetch(path)
+    .then((res) => res.text())
+    .then((html) => {
+      if (html !== cached) {
+        $(selector).innerHTML = html;
+        localStorage.setItem(path, html);
+      }
+    })
+    .finally(() => {
+      window.dispatchEvent(new Event("template-loaded"));
+    });
+}
