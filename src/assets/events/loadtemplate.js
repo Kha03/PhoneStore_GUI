3cd01c6551b561dbc01f0dbc0fb1fd8b8219664f
Kha <minhkha20031996@gmail.com>
2024-11-16 15:59:06 +0700

    update loadtemlate

--- src/assets/events/loadtemplate.js
+++ src/assets/events/loadtemplate.js
@@ -1,5 +1,5 @@
-const $ = document.querySelector.bind(document);
-const $$ = document.querySelectorAll.bind(document);
+const querySelector = document.querySelector.bind(document);
+const querySelectorAll = document.querySelectorAll.bind(document);
 
 /**
  * Hàm tải template
@@ -13,14 +13,14 @@ const $$ = document.querySelectorAll.bind(document);
 function load(selector, path) {
   const cached = localStorage.getItem(path);
   if (cached) {
-    $(selector).innerHTML = cached;
+    querySelector(selector).innerHTML = cached;
   }
 
   fetch(path)
     .then((res) => res.text())
     .then((html) => {
       if (html !== cached) {
-        $(selector).innerHTML = html;
+        querySelector(selector).innerHTML = html;
         localStorage.setItem(path, html);
       }
     })
